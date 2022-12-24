ace89a8ec583f792e60f0164d364ba5dd47c3ba1
bmurdock <bmurdock@gmail.com>
2022-12-24 00:50:44 +0000

    WIP: trying to just get tokens, still failing, coding on phone is hard

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pathlib
 import sys
 import argparse
+import string 
 
 def parse_args():
   parser = argparse.ArgumentParser()
@@ -9,10 +10,42 @@ def parse_args():
   parser.add_argument('-v', '--verbose', action='store_true')
   return parser.parse_args()
 
+def next_token(code, position, line):
+  if code[position] == '\n':
+    line += 1
+    position+=1
+    
+  while code[position] in string.whitespace:
+    position += 1
+
+  if code[position] == ';':
+    position+= 1
+    print(';')
+    return position, line 
+    
+  if(code[position] in (string.ascii_letters + '_' +'$')):
+       token_start = position 
+       while(code[position] in (string.ascii_letters + string.digits + '_')):
+         position += 1
+       print(code[token_start: position])
+       return position, line
+
 def main(args):
+  keywords = ['begin',
+              'end',
+              'module',
+              'endmodule',
+              'initial',]
+  built_in = ['$display']
+  sv_code = ''
+  position = 0
+  line = 1
   with open(args.filename) as sv_file:
-    for line in sv_file:
-      print(line.rstrip())
+    sv_code = sv_file.read()
+    print(sv_code[-1])
+    sys.exit(-1)
+  while position < len(sv_code):
+    position, line = next_token(sv_code, position, line)
   return 0
   
 if __name__ == '__main__':
