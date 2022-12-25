0aca803da3a24bfa12d13196df310d3dbd8b2464
Bryan Murdock <bmurdock@gmail.com>
2022-12-24 23:05:04 -0700

    there, we tokenized all of "hello, world"

--- main.py
+++ main.py
@@ -13,24 +13,48 @@ def parse_args():
 
 
 def next_token(code, position, line):
-    if code[position] == '\n':
-        line += 1
-        position+=1
+    try:
+        # handle newline
+        if code[position] == '\n':
+            line += 1
+            position+=1
 
-    while code[position] in string.whitespace:
-        position += 1
-
-    if code[position] == ';':
-        position+= 1
-        print(';')
-        return position, line
-
-    if(code[position] in (string.ascii_letters + '_' + '$')):
-        token_start = position
-        position += 1
-        while(code[position] in (string.ascii_letters + string.digits + '_')):
+        # handle whitespace
+        while code[position] in string.whitespace:
             position += 1
-        print(code[token_start: position])
+
+        # handle punctuation
+        if code[position] in ';()':
+            position+= 1
+            print(f'>{code[position-1]}<')
+            return position, line
+
+        # handle string constants
+        if code[position] == '"':
+            string_start = position
+            position += 1
+            # look for quotes to end string, but not if they are preceded by a \
+            while code[position] != '"':
+                if code[position] == '\\':
+                    position += 2
+                else:
+                    position += 1
+            position += 1
+            print(f'>{code[string_start:position]}<')
+            return position, line
+
+        # handle identifiers
+        if(code[position] in (string.ascii_letters + '_' + '$')):
+            token_start = position
+            position += 1
+            while(code[position] in (string.ascii_letters + string.digits + '_')):
+                position += 1
+            print(f'>{code[token_start:position]}<')
+            return position, line
+    # If any of the above position += 1 take us past the end of code,
+    # this will save us.  Some would argue this is an improper use of
+    # exceptions.
+    except IndexError as err:
         return position, line
 
 
