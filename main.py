6066add9e951459128487410ed9d6d8264f986aa
Bryan Murdock <bmurdock@gmail.com>
2022-12-24 22:40:38 -0700

    whitespace cleanup from laptop

--- main.py
+++ main.py
@@ -1,53 +1,57 @@
 #!/usr/bin/env python
-import pathlib
-import sys
 import argparse
-import string 
+import pathlib
+import string
+import sys
+
 
 def parse_args():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('filename')
-  parser.add_argument('-v', '--verbose', action='store_true')
-  return parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    return parser.parse_args()
+
 
 def next_token(code, position, line):
-  if code[position] == '\n':
-    line += 1
-    position+=1
-    
-  while code[position] in string.whitespace:
-    position += 1
+    if code[position] == '\n':
+        line += 1
+        position+=1
 
-  if code[position] == ';':
-    position+= 1
-    print(';')
-    return position, line 
-    
-  if(code[position] in (string.ascii_letters + '_' +'$')):
-       token_start = position 
-       position += 1
-       while(code[position] in (string.ascii_letters + string.digits + '_')):
-         position += 1
-       print(code[token_start: position])
-       return position, line
+    while code[position] in string.whitespace:
+        position += 1
+
+    if code[position] == ';':
+        position+= 1
+        print(';')
+        return position, line
+
+    if(code[position] in (string.ascii_letters + '_' + '$')):
+        token_start = position
+        position += 1
+        while(code[position] in (string.ascii_letters + string.digits + '_')):
+            position += 1
+        print(code[token_start: position])
+        return position, line
+
 
 def main(args):
-  keywords = ['begin',
-              'end',
-              'module',
-              'endmodule',
-              'initial',]
-  built_in = ['$display']
-  sv_code = ''
-  position = 0
-  line = 1
-  with open(args.filename) as sv_file:
-    sv_code = sv_file.read()
-    print(sv_code[-1])
-    sys.exit(-1)
-  while position < len(sv_code):
-    position, line = next_token(sv_code, position, line)
-  return 0
-  
+    keywords = ['begin',
+                'end',
+                'module',
+                'endmodule',
+                'initial',]
+    built_in = ['$display']
+    sv_code = ''
+    position = 0
+    line = 1
+    with open(args.filename) as sv_file:
+        sv_code = sv_file.read()
+        print(sv_code[-1])
+        sys.exit(-1)
+    while position < len(sv_code):
+        position, line = next_token(sv_code, position, line)
+    return 0
+
+
 if __name__ == '__main__':
-  sys.exit(main(parse_args()))
+    sys.exit(main(parse_args()))
