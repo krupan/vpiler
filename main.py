820993725397cd51aeeeb6c760d3f53188ec0d15
Bryan Murdock <bmurdock@gmail.com>
2022-12-25 19:49:06 -0700

    WIP: start on parsing

--- main.py
+++ main.py
@@ -12,50 +12,102 @@ def parse_args():
     return parser.parse_args()
 
 
-def next_token(code, position, line):
-    try:
-        # handle newline
-        if code[position] == '\n':
-            line += 1
-            position+=1
+class Tokenizer:
+    def __init__(self, sv_code):
+        self.code = sv_code
+        self.position = 0
+        self.line = 1
 
-        # handle whitespace
-        while code[position] in string.whitespace:
-            position += 1
+    def next(self):
+        try:
+            # handle newline
+            if self.code[self.position] == '\n':
+                self.line += 1
+                self.position+=1
 
-        # handle punctuation
-        if code[position] in ';()':
-            position+= 1
-            print(f'>{code[position-1]}<')
-            return position, line
+            # handle whitespace
+            while self.code[self.position] in string.whitespace:
+                self.position += 1
 
-        # handle string constants
-        if code[position] == '"':
-            string_start = position
-            position += 1
-            # look for quotes to end string, but not if they are preceded by a \
-            while code[position] != '"':
-                if code[position] == '\\':
-                    position += 2
-                else:
-                    position += 1
-            position += 1
-            print(f'>{code[string_start:position]}<')
-            return position, line
+            # handle punctuation
+            if self.code[self.position] in ';()':
+                self.position+= 1
+                return self.code[self.position-1]
 
-        # handle identifiers
-        if(code[position] in (string.ascii_letters + '_' + '$')):
-            token_start = position
-            position += 1
-            while(code[position] in (string.ascii_letters + string.digits + '_')):
-                position += 1
-            print(f'>{code[token_start:position]}<')
-            return position, line
-    # If any of the above position += 1 take us past the end of code,
-    # this will save us.  Some would argue this is an improper use of
-    # exceptions.
-    except IndexError as err:
-        return position, line
+            # handle string constants
+            if self.code[self.position] == '"':
+                string_start = self.position
+                self.position += 1
+                # look for quotes to end string, but not if they are preceded by a \
+                while self.code[self.position] != '"':
+                    if self.code[self.position] == '\\':
+                        self.position += 2
+                    else:
+                        self.position += 1
+                self.position += 1
+                return self.code[string_start:self.position]
+
+            # handle identifiers
+            if(self.code[self.position] in (string.ascii_letters + '_' + '$')):
+                token_start = self.position
+                self.position += 1
+                while(self.code[self.position] in (string.ascii_letters + string.digits + '_')):
+                    self.position += 1
+                return self.code[token_start:self.position]
+        # If any of the above position += 1 take us past the end of code,
+        # this will save us.  Some would argue this is an improper use of
+        # exceptions.
+        except IndexError as err:
+            return ''
+
+
+# parser functions, see systemverilog-1800-2018.bnf that is included
+# along with this code
+class Parser:
+    def __init__(self, filename):
+        self.filename = filename
+        with open(self.filename) as sv_file:
+            self.code = sv_file.read()
+        self.tokenizer = Tokenizer(self.code)
+        self.token = ''
+
+    def error(self, message):
+        print(f'{self.filename}:{self.tokenizer.line}:{self.tokenizer.position}: error: {message}')
+
+    def go(self):
+        self.source_text()
+
+    def source_text(self):
+        self.token = self.tokenizer.next()
+        while self.token != '':
+            self.description():
+
+    def description(self):
+        self.module_declaration()
+
+    def module_declaration(self):
+        self.token = self.tokenizer.next()
+        self.module_ansi_header()
+        self.token = self.tokenizer.next()
+        self.non_port_module_item()
+        self.token = self.tokenizer.next()
+        if self.token != 'endmodule':
+            self.error("expected 'endmodule'")
+
+    def module_ansi_header(self):
+        self.token = self.tokenizer.next()
+        if self.token != 'module':
+            self.error("expected 'module'")
+        self.token = self.tokenizer.next()
+        self.module_identifer()
+        self.token = self.tokenizer.next()
+        if self.token != ';':
+            self.error("expected ';'")
+
+    def non_port_module_item(self):
+        # WIP TODO
+        pass
+        
 
 
 def main(args):
@@ -65,13 +117,8 @@ def main(args):
                 'endmodule',
                 'initial',]
     built_in = ['$display']
-    sv_code = ''
-    position = 0
-    line = 1
-    with open(args.filename) as sv_file:
-        sv_code = sv_file.read()
-    while position < len(sv_code):
-        position, line = next_token(sv_code, position, line)
+    parser = Parser(args.filename)
+    parser.go()
     return 0
 
 
